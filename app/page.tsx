19869dd22da287a9cd6ea2ea1921ffbb91f0f111
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-09-21 17:59:23 +0200

    feat(content): retrieving content from external json

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 
-export default function Home() {
+import { Content } from './types';
+
+export default async function Home() {
+  const contentUrl = process.env.CONTENT_JSON_URL as string;
+  const response = await fetch(contentUrl);
+  const content: Content = await response.json();
+
   return (
     <main>
       <Image src="/logo.svg" alt="logo" width={150} height={100} />
-      <h1>Cuenta única</h1>
+      <h1>{content.title}</h1>
+      <p>{content.description}</p>
     </main>
   );
 }

94e962d10d052cb1c9e5ee24c8dbc1d1ae7ee4e5
je1999 <je99alvarez@gmail.com>
2023-09-29 09:34:11 -0400

    feat: home page component
    initial component where the other component sections will be called

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from 'next/image';
+import { ThemeProvider } from '@ogticrd/ui-kit';
 
 import { Content } from './types';
+
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Description from './components/description';
+import Benefits from './components/benefits';
+import StepsToRegister from './components/steps-to-register';
+import OnlineServices from './components/online-services';
+import Footer from './components/footer';
 
 export default async function Home() {
   const contentUrl = process.env.CONTENT_JSON_URL as string;
@@ -8,10 +17,17 @@ export default async function Home() {
   const content: Content = await response.json();
 
   return (
-    <main>
-      <Image src="/logo.svg" alt="logo" width={150} height={100} />
-      <h1>{content.title}</h1>
-      <p>{content.description}</p>
-    </main>
+    <div>
+      <Navbar />
+      <Hero />
+      <Description />
+      <Benefits />
+      <StepsToRegister />
+      <OnlineServices />
+      <Footer />
+
+      {/* <h1>{content.title}</h1>
+      <p>{content.description}</p> */}
+    </div>
   );
 }

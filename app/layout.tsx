b38b468c4e45784f5be62a830d62e3857e1b8852
je1999 <je99alvarez@gmail.com>
2023-09-29 09:25:59 -0400

    feat: theme config
    official header was imported and the theme configuration was added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import OfficialHeader from './components/official-header';
+import { ThemeProvider } from './theme';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={inter.className}>
+        <ThemeProvider>
+          <OfficialHeader />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
